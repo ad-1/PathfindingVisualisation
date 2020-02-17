1181885232e341e3a9121e22361d69ff61278e86
AndyJ <andrewdaviesul@gmail.com>
2020-02-17 21:33:49 +0000

    tkk methods updated

--- graph.py
+++ graph.py
@@ -1,6 +1,6 @@
 # Graph in Tkinter for Pathfinding Visualisation
-import shutil
 
+import shutil
 import requests
 import tkinter as tk
 import random
@@ -22,8 +22,8 @@ class Graph:
     def __init__(self):
         """ Init graphical user interface for pathfinding """
 
-        self.algorithms = ['Breadth First Search', 'Depth First Search', 'Dijkstra', 'A*']
-        self.solve_mode = 0
+        self.algorithms = ['Breadth First Search', 'BFS Recursive', 'Depth First Search', 'DFS Recursive', 'Dijkstra', 'A*']
+        self.solve_mode = 3
         self.draw_modes = [1, 2, 3]
         self.draw_mode = 1
         self.is_solving = False
@@ -35,7 +35,7 @@ class Graph:
         self.graph_width = 1220
         self.graph_height = 850
         self.canvas = tk.Canvas(self.root, height=self.graph_height + 50, width=self.graph_width, bg='#333333')
-        self.node_size = 20
+        self.node_size = 50
         self.n_cols = None
         self.n_rows = None
         self.nodes = None
@@ -88,7 +88,7 @@ class Graph:
                 x1 = j * self.node_size
                 x2 = x1 + self.node_size
                 node = Node(x1, y1, x2, y2, i, j, idx, self)
-                progress_state(node, [], State.normal, self.render_delay)
+                progress_state(node, [], State.NORMAL, self.render_delay)
                 self.nodes[i][j] = node
                 idx += 1
         self.flat_nodes = [node for row in self.nodes for node in row]
@@ -104,32 +104,35 @@ class Graph:
             return
         self.reset_stored_nodes()
         for node in self.flat_nodes:
-            progress_state(node, [], State.normal, self.render_delay)
+            progress_state(node, [], State.NORMAL, self.render_delay)
 
     def clear_walls(self):
         """ clear all walls from graph """
         if self.is_solving:
             return
         for node in self.flat_nodes:
-            if node.state == State.wall:
-                progress_state(node, [], State.normal, self.render_delay)
+            if node.state == State.WALL:
+                progress_state(node, [], State.NORMAL, self.render_delay)
 
     def clear_path(self):
         """ clear any nodes in last iteration """
         if self.is_solving:
             return
         for node in self.flat_nodes:
-            if node.state == State.visited or \
-                    node.state == State.visiting or \
-                    node.state == State.queue or \
-                    node.state == State.path:
-                progress_state(node, [], State.normal, self.render_delay)
+            if node.state == State.VISITED or \
+                    node.state == State.VISITING or \
+                    node.state == State.QUEUE or \
+                    node.state == State.PATH:
+                progress_state(node, [], State.NORMAL, self.render_delay)
 
     def display_logo(self):
         """ display images for selected algorithms """
         url = 'https://fbcd.co/images/products/f004c91f1144880ca9a6bc83f3ae61f4_resize.jpg'
         file_name = 'images/logo.png'
-        response = requests.get(url, stream=True)
+        try:
+            response = requests.get(url, stream=True)
+        except requests.ConnectionError:
+            return
         with open(file_name, 'wb') as file:
             shutil.copyfileobj(response.raw, file)
         del response
@@ -140,62 +143,62 @@ class Graph:
     def config_menu(self):
         """ menu - user configurable settings for visualisation"""
         self.menu_frame.grid()
-        menu_width, padx, pady = 18, 10, 5
+        menu_width = 18
+        self.tkk_btn(self.menu_frame, [('Visualise', self.validate_graph)], True)
         self.display_logo()
         current_algo = tk.StringVar()
         current_algo.set(self.algorithms[self.solve_mode])
         algorithms_menu = tk.OptionMenu(self.menu_frame, current_algo, *self.algorithms, command=self.algorithm_change)
         algorithms_menu.config(width=menu_width)
-        node_size_label = tk.Label(self.menu_frame, text='Node Size', anchor='w')
-        node_size_scaler = tk.Scale(self.menu_frame,
-                                    from_=20,
-                                    to=60,
-                                    orient=tk.HORIZONTAL,
-                                    command=self.change_node_size,
-                                    resolution=2)
-        tkk.Button(self.menu_frame, text='Clear Graph', command=self.clear_graph)
-        tkk.Button(self.menu_frame, text='Clear Wall', command=self.clear_walls)
-        tkk.Button(self.menu_frame, text='Clear Path', command=self.clear_path)
-        tkk.Button(self.menu_frame, text='Generate Random Maze', command=self.random_maze)
-        wall_frequency_label = tk.Label(self.menu_frame, text='Wall Frequency', anchor='w')
-        wall_frequency_scaler = tk.Scale(self.menu_frame,
-                                         from_=0.05,
-                                         to=0.5,
-                                         orient=tk.HORIZONTAL,
-                                         command=self.change_wall_frequency,
-                                         resolution=0.01)
+        self.tkk_label(self.menu_frame, ['Node Size'])
+        self.tkk_scaler(20, 60, self.change_node_size)
+        self.tkk_label(self.menu_frame, ['Wall Frequency'])
+        self.tkk_scaler(0.05, 0.5, self.change_wall_frequency)
+        self.tkk_btn(self.menu_frame, [('Generate Random Maze', self.random_maze),
+                                       ('Clear Graph', self.clear_graph),
+                                       ('Clear Wall', self.clear_walls),
+                                       ('Clear Path', self.clear_path)], True)
         rb = tk.IntVar()
         rb.set(self.draw_mode)
-        rb_start = tk.Radiobutton(self.menu_frame,
-                                  text='Place Start Node',
-                                  variable=rb,
-                                  value=self.draw_modes[0],
-                                  command=lambda: self.change_draw_mode(rb.get()))
-        tk.Radiobutton(self.menu_frame,
-                       text='Place Finish Node',
-                       variable=rb,
-                       value=self.draw_modes[1],
-                       command=lambda: self.change_draw_mode(rb.get()))
-        tk.Radiobutton(self.menu_frame,
-                       text='Draw Weighted Node',
-                       variable=rb,
-                       value=self.draw_modes[2],
-                       command=lambda: self.change_draw_mode(rb.get()))
-        visualise_button = tkk.Button(self.menu_frame, text='Visualise', command=self.validate_graph)
+        self.tkk_rbtn([('Place Start Node', rb), ('Place Finish Node', rb), ('Draw Weighted Node', rb)])
+        self.tkk_label(self.menu_frame, ['Graph Legend'])
+        state_keys = [(state.name, state.value) for state in State]
+        self.tkk_btn(self.menu_frame, state_keys, False)
         for r, child in enumerate(self.menu_frame.winfo_children()):
-            if child == node_size_label or child == wall_frequency_label:
-                child.grid_configure(row=r, column=0, sticky='ew', pady=(10, 0), padx=padx)
-            elif child == node_size_scaler or child == wall_frequency_scaler:
-                child.grid_configure(row=r, column=0, sticky='ew', pady=(0, 10), padx=padx)
-            elif isinstance(child, tk.Radiobutton):
-                if child == rb_start:
-                    child.grid_configure(row=r, column=0, sticky='ew', pady=(12, 0), padx=padx)
-                else:
-                    child.grid_configure(row=r, column=0, sticky='ew', pady=0, padx=padx)
-            elif child == visualise_button:
-                child.grid_configure(row=r, column=0, sticky='ew', pady=15, padx=padx)
+            pad = 0 if isinstance(child, tk.Button) else 5
+            child.grid_configure(row=r, column=0, sticky='ew', padx=pad, pady=pad)
+
+    @staticmethod
+    def tkk_btn(parent, btns, is_tkk):
+        """ create buttons given list of text and commands """
+        for text, cmd in btns:
+            if is_tkk:
+                tkk.Button(parent, text=text, command=cmd)
             else:
-                child.grid_configure(row=r, column=0, sticky='ew', pady=pady, padx=padx)
+                tk.Button(parent, text=text, highlightbackground=cmd, height=1, state='disabled')
+
+    @staticmethod
+    def tkk_label(parent, text):
+        """ create labels from list of text """
+        for t in text:
+            tkk.Label(parent, text=t, anchor='e')
+
+    def tkk_rbtn(self, rbtns):
+        """ create radio buttons given text, mode and cmd """
+        for i, (text, var) in enumerate(rbtns):
+            tkk.Radiobutton(self.menu_frame,
+                            text=text,
+                            variable=var,
+                            value=i + 1,
+                            command=lambda: self.change_draw_mode(var.get()))
+
+    def tkk_scaler(self, from_, to_, cmd):
+        """ create themed scaler """
+        tkk.Scale(self.menu_frame,
+                  from_=from_,
+                  to=to_,
+                  orient=tk.HORIZONTAL,
+                  command=cmd)
 
     def algorithm_change(self, *args):
         """ change solve mode """
@@ -207,9 +210,9 @@ class Graph:
             return
         for node in self.flat_nodes:
             if random.uniform(0, 1) < self.wall_frequency:
-                progress_state(node, [self.start_node, self.finish_node], State.wall, self.render_delay)
+                progress_state(node, [self.start_node, self.finish_node], State.WALL, self.render_delay)
             else:
-                progress_state(node, [self.start_node, self.finish_node], State.normal, self.render_delay)
+                progress_state(node, [self.start_node, self.finish_node], State.NORMAL, self.render_delay)
 
     def change_wall_frequency(self, val):
         """ change probability a wall will appear for random maze """
@@ -278,22 +281,22 @@ class Graph:
     def update_start(self, node):
         """ update starting node """
         if self.start_node is not None:
-            progress_state(self.start_node, [self.finish_node], State.normal, self.render_delay)
-        progress_state(node, [self.finish_node], State.start, self.render_delay)
+            progress_state(self.start_node, [self.finish_node], State.NORMAL, self.render_delay)
+        progress_state(node, [self.finish_node], State.START, self.render_delay)
         self.start_node = node
 
     def update_finish(self, node):
         """ update finishing node """
         if self.finish_node is not None:
-            progress_state(self.finish_node, [self.start_node], State.normal, self.render_delay)
-        progress_state(node, [self.start_node], State.finish, self.render_delay)
+            progress_state(self.finish_node, [self.start_node], State.NORMAL, self.render_delay)
+        progress_state(node, [self.start_node], State.FINISH, self.render_delay)
         self.finish_node = node
 
     @staticmethod
     def update_wall(node):
         """ update node as wall or not """
-        if node.state != State.start and node.state != State.finish:
-            node.state = (State.wall, 0) if node.state != State.wall else (State.normal, 0)
+        if node.state != State.START and node.state != State.FINISH:
+            node.state = (State.WALL, 0) if node.state != State.WALL else (State.NORMAL, 0)
             node.cost = inf if node.cost == 1 else inf
 
     def validate_graph(self):
@@ -305,11 +308,13 @@ class Graph:
         """ solve using selected algorithm """
         self.clear_path()
         self.is_solving = True
-        if self.solve_mode == 0:
-            BFS(self.nodes, self.start_node, self.finish_node, self)
-        elif self.solve_mode == 1:
-            DFS(self.nodes, self.start_node, self.finish_node, self)
+        if self.solve_mode == 0 or self.solve_mode == 1:
+            bfs_recursive = True if self.solve_mode == 1 else False
+            BFS(self.nodes, bfs_recursive, self.start_node, self.finish_node, self)
         elif self.solve_mode == 2 or self.solve_mode == 3:
-            a_star = True if self.solve_mode == 3 else False
+            dfs_recursive = True if self.solve_mode == 3 else False
+            DFS(self.nodes, dfs_recursive, self.start_node, self.finish_node, self)
+        elif self.solve_mode == 4 or self.solve_mode == 5:
+            a_star = True if self.solve_mode == 5 else False
             Dijkstra(self.nodes, a_star, self.start_node, self.finish_node, self)
         self.is_solving = False
