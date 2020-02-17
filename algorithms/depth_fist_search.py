0eb8abd0d9d17d8b6ebfc18f63afcead46928caa
AndyJ <andrewdaviesul@gmail.com>
2020-02-17 00:06:48 +0000

    dfs recursive

--- algorithms/depth_fist_search.py
+++ algorithms/depth_fist_search.py
@@ -1,6 +1,7 @@
 # Depth First Search
 
-from adj_list import get_adj_list
+from helpers.adj_list import get_adj_list
+from helpers.progress_state import progress_state
 from state import State
 
 
@@ -11,24 +12,20 @@ class DFS:
         self.subscriber = root
         self.nodes = nodes
         self.adjacency_list = get_adj_list(nodes)
-        self.visited = []
         self.path = []
+        self.start_node = start_node
         self.finish_node = finish_node
+        self.state_consts = [start_node, finish_node]
+        self.render_delay = 0.01
         if self.dfs_recursive(start_node):
             self.backtrack()
 
-    def dispatch(self, node):
-        """ dispatch node info """
-        self.subscriber.render_node(node)
-
     def dfs_recursive(self, node):
         """ depth first search algorithm """
-        if node == self.finish_node:
+        if node == self.finish_node or node not in self.adjacency_list:
             return True
-        if not node.visited:
-            node.state = State.visited
-            self.visited.append(node)
-            self.dispatch(node)
+        if not node.state == State.visited:
+            progress_state(node, self.state_consts, State.visited, self.render_delay)
             edges = self.adjacency_list[node]
             for edge in edges:
                 if self.dfs_recursive(edge):
@@ -39,5 +36,4 @@ class DFS:
     def backtrack(self):
         """ backtrack through path to finish node """
         for node in reversed(self.path):
-            node.state = State.path
-            self.dispatch(node)
+            progress_state(node, self.state_consts, State.path, self.render_delay)
